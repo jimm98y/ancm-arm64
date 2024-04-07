e914382470d2225c7ee5e1feede3d01e47a6237a
Lukas Volf <jimm98y@hotmail.com>
2024-04-07 15:14:41 +0200

    B Fixed uninstall

--- src/dotnet-hosting-win-arm64/fix2.bat
+++ src/dotnet-hosting-win-arm64/fix2.bat
@@ -1,11 +1,9 @@
-cd %~dp0
-
 if "%~1"=="install" (
-    move /y "%ProgramFiles%\IIS\Asp.Net Core Module\V2\aspnetcorev2.dll" "%ProgramFiles%\IIS\Asp.Net Core Module\V2\aspnetcorev2_arm64.dll"
-    move /y "%ProgramFiles%\IIS\Asp.Net Core Module\V2\%~2\aspnetcorev2_outofprocess.dll" "%windir%\system32\inetsrv\aspnetcorev2_outofprocess_arm64.dll"
+    move "%ProgramFiles%\IIS\Asp.Net Core Module\V2\aspnetcorev2.dll" "%ProgramFiles%\IIS\Asp.Net Core Module\V2\aspnetcorev2_arm64.dll"
+    move "%ProgramFiles%\IIS\Asp.Net Core Module\V2\%~2\aspnetcorev2_outofprocess.dll" "%windir%\system32\inetsrv\aspnetcorev2_outofprocess_arm64.dll"
 )
 
 if "%~1"=="uninstall" (
-	move /y "%ProgramFiles%\IIS\Asp.Net Core Module\V2\aspnetcorev2_arm64.dll" "%ProgramFiles%\IIS\Asp.Net Core Module\V2\aspnetcorev2.dll"
-    move /y "%windir%\system32\inetsrv\aspnetcorev2_outofprocess_arm64.dll" "%ProgramFiles%\IIS\Asp.Net Core Module\V2\%~2\aspnetcorev2_outofprocess.dll"
+	move "%ProgramFiles%\IIS\Asp.Net Core Module\V2\aspnetcorev2_arm64.dll" "%ProgramFiles%\IIS\Asp.Net Core Module\V2\aspnetcorev2.dll"
+    del "%windir%\system32\inetsrv\aspnetcorev2_outofprocess_arm64.dll"
 )
--- src/dotnet-hosting-win-arm64/fix3.bat
+++ src/dotnet-hosting-win-arm64/fix3.bat
@@ -2,13 +2,13 @@ cd %~dp0
 
 if "%~1"=="install" (
 msiexec /a "%~dp0AspNetCoreModuleV2_x64.msi" /qn TARGETDIR="%~dp0\msi_x64"
-move /y "%ProgramFiles(x86)%\IIS\Asp.Net Core Module\V2\aspnetcorev2.dll" "%ProgramFiles(x86)%\IIS\Asp.Net Core Module\V2\aspnetcorev2.dll.bak"
+move "%ProgramFiles(x86)%\IIS\Asp.Net Core Module\V2\aspnetcorev2.dll" "%ProgramFiles(x86)%\IIS\Asp.Net Core Module\V2\aspnetcorev2.dll.bak"
 copy "%~dp0\msi_x64\IIS\Asp.Net Core Module\WowOnly\aspnetcorev2.dll" "%ProgramFiles(x86)%\IIS\Asp.Net Core Module\V2\aspnetcorev2.dll"
-move /y "%ProgramFiles(x86)%\IIS\Asp.Net Core Module\V2\%~2\aspnetcorev2_outofprocess.dll" "%ProgramFiles(x86)%\IIS\Asp.Net Core Module\V2\%~2\aspnetcorev2_outofprocess.dll.bak"
+move "%ProgramFiles(x86)%\IIS\Asp.Net Core Module\V2\%~2\aspnetcorev2_outofprocess.dll" "%ProgramFiles(x86)%\IIS\Asp.Net Core Module\V2\%~2\aspnetcorev2_outofprocess.dll.bak"
 copy "%~dp0\msi_x64\IIS\Asp.Net Core Module\WowOnly\WowOnly\aspnetcorev2_outofprocess.dll" "%ProgramFiles(x86)%\IIS\Asp.Net Core Module\V2\%~2\aspnetcorev2_outofprocess.dll"
 )
 
 if "%~1"=="uninstall" (
-	move /y "%ProgramFiles(x86)%\IIS\Asp.Net Core Module\V2\aspnetcorev2.dll.bak" "%ProgramFiles(x86)%\IIS\Asp.Net Core Module\V2\aspnetcorev2.dll"
-    move /y "%ProgramFiles(x86)%\IIS\Asp.Net Core Module\V2\%~2\aspnetcorev2_outofprocess.dll.bak" "%ProgramFiles(x86)%\IIS\Asp.Net Core Module\V2\%~2\aspnetcorev2_outofprocess.dll"
+	move "%ProgramFiles(x86)%\IIS\Asp.Net Core Module\V2\aspnetcorev2.dll.bak" "%ProgramFiles(x86)%\IIS\Asp.Net Core Module\V2\aspnetcorev2.dll"
+    move "%ProgramFiles(x86)%\IIS\Asp.Net Core Module\V2\%~2\aspnetcorev2_outofprocess.dll.bak" "%ProgramFiles(x86)%\IIS\Asp.Net Core Module\V2\%~2\aspnetcorev2_outofprocess.dll"
 )
